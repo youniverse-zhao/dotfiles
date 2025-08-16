40713a7d8c71e57b29aa8927fb013fc232f08b91
Youniverse <youniverse.zhao@gmail.com>
2025-08-16 22:33:46 +1000

    Fix incorrect rmdir flag

--- undo/custom-dirs.sh
+++ undo/custom-dirs.sh
@@ -9,7 +9,7 @@ folder_names=("Captures" "Vault" "Notes")
 for folder_name in ${folder_names[@]}
 do
     if [ -d "$HOME/$folder_name" ]; then
-        rmdir -p "$HOME/$folder_name"
+        rmdir "$HOME/$folder_name"
         echo "Folder $folder_name has been deleted"
     else
         echo "Folder $folder_name is not available"
