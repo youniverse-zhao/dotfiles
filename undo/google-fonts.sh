1eb1ebceacc73ec3985b12ec9051853dd8421ca1
Youniverse <youniverse.zhao@gmail.com>
2025-08-16 21:04:56 +1000

    Update Google Fonts deletion process

--- undo/google-fonts.sh
+++ undo/google-fonts.sh
@@ -9,7 +9,7 @@ PATH_TO_GOOGLE_FONTS_DIR="$HOME/Library/Fonts/$GOOGLE_FONTS_DIR"
 
 if [ -d $PATH_TO_GOOGLE_FONTS_DIR ]; then
     echo "Directory $GOOGLE_FONTS_DIR exists, removing..."
-    rmdir $PATH_TO_GOOGLE_FONTS_DIR 
+    rm -rf $PATH_TO_GOOGLE_FONTS_DIR 
 else
     echo "Directory $GOOGLE_FONTS_DIR does not exists, nothing to do."
 fi
