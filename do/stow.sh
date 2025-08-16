8283b8b5710f0ae7133cf5dc076a0ffb9b85ae16
Youniverse <youniverse.zhao@gmail.com>
2025-08-16 20:42:24 +1000

    Add backup process to stow

--- do/stow.sh
+++ do/stow.sh
@@ -5,4 +5,26 @@
 #!/bin/bash
 
 brew install stow
-cd "$HOME/dotfiles"; stow dots
+
+# Enable nullglob to ensure that if no files match the pattern,
+# the glob expands to nothing instead of the pattern itself.
+shopt -s nullglob
+
+project_root_dir="$HOME/dotfiles"
+
+# Create ./original to backup existing dot files
+path_to_backup_dir="$project_root_dir/original"
+mkdir -p "$path_to_backup_dir"
+
+items=("$project_root_dir"/.*)
+for item in ${items[@]} ; 
+do
+    filename=$(basename "$item")
+    path_to_existing_dot_file="$HOME/$filename"
+    if [ -f "$path_to_existing_dot_file" ] || [ -d "$path_to_existing_dot_file" ]; then
+        mv "$path_to_existing_dot_file" "$path_to_backup_dir/"
+        echo "$filename has been backup."
+    fi
+done
+
+cd "$project_root_dir"; stow dots
--- undo/stow.sh
+++ undo/stow.sh
@@ -5,3 +5,18 @@
 #!/bin/bash
 
 cd "$HOME/dotfiles"; stow -D dots
+
+project_root_dir="$HOME/dotfiles"
+path_to_backup_dir="$project_root_dir/original"
+
+items=("$path_to_backup_dir"/.*)
+for item in ${items[@]} ; 
+do
+    filename=$(basename "$item")
+    path_to_target_dot_file="$HOME/$filename"
+    if [ -f "$path_to_target_dot_file" ] || [ -d "$path_to_target_dot_file" ]; then
+        echo "There is an $filename in ~/, please check manually."
+    else
+        mv "$path_to_backup_dir/$filename" "$HOME/"
+    fi
+done
